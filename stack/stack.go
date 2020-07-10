c7e76a9773ea5e52492d278e526f124ac35b9a47
mrkish <ahoythar@gmail.com>
2020-07-10 08:48:09 -0500

    adding some starting code for stack

--- stack/stack.go
+++ stack/stack.go
@@ -1,3 +1,23 @@
 package stack
 
-type Stack struct{}
+import (
+	"errors"
+	"go-datastructures/model"
+)
+
+type Stack struct {
+	List *SinglyLinkedList
+}
+
+func (s *Stack) Pop() model.Object {
+	out := s.List.Head.Value
+	err := s.List.Remove(out)
+	if err != nil {
+		return errors.New("List is empty!")
+	}
+	return out
+}
+
+func (s *Stack) Add(obj model.Object) {
+	s.List.Add(obj)
+}
