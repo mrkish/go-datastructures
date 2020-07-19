0fc0bda6c44fb75903e69879c625cf40e41fe652
mrkish <ahoythar@gmail.com>
2020-07-19 18:32:37 -0500

    added test for adding to the queue

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -15,19 +15,19 @@ func TestQueue_Dequeue(t *testing.T) {
 		want     model.Object
 		wantErr  bool
 	}{
-		// {
-		// 	name: "item is added to existing Queue",
-		// 	values: []string{
-		// 		"first",
-		// 		"second",
-		// 	},
-		// 	expected: []string{
-		// 		"first",
-		// 		"second",
-		// 		"last",
-		// 	},
-		// 	want: model.Object{Value: "first"},
-		// },
+		{
+			name: "item is added to existing Queue",
+			values: []string{
+				"first",
+				"second",
+			},
+			expected: []string{
+				"first",
+				"second",
+				"last",
+			},
+			want: model.Object{Value: "first"},
+		},
 		{
 			name: "first value is returned and queue is then empty",
 			values: []string{
@@ -56,6 +56,64 @@ func TestQueue_Dequeue(t *testing.T) {
 			if tt.want.Value != "" {
 				if q.List.Find(tt.want) {
 					t.Error("Stack.Pop() popped item not removed from stack")
+				}
+			}
+		})
+	}
+}
+
+func TestQueue_Add(t *testing.T) {
+	type args struct {
+		obj model.Object
+	}
+	tests := []struct {
+		name     string
+		values   []string
+		expected []string
+		args     args
+	}{
+		{
+			name: "item is added to existing Queue",
+			values: []string{
+				"first",
+				"second",
+			},
+			expected: []string{
+				"first",
+				"second",
+				"last",
+			},
+			args: args{
+				model.Object{Value: "last"},
+			},
+		},
+		{
+			name: "item is added to a new stack",
+			expected: []string{
+				"first",
+			},
+			args: args{
+				model.Object{Value: "first"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &linkedlist.DoublyLinkedList{}
+			l.AddNode(linkedlist.BuildDoubleNodes(tt.values)...)
+			expected := &linkedlist.DoublyLinkedList{}
+			expected.AddNode(linkedlist.BuildDoubleNodes(tt.expected)...)
+			q := &Queue{
+				List: l,
+			}
+			q.Add(tt.args.obj)
+			if !reflect.DeepEqual(q.List.Tail.Value, tt.args.obj) {
+				t.Error("Add() did not add values as expected")
+			}
+			// Check that the links are intact
+			if q.List.Tail.Next != nil {
+				if !reflect.DeepEqual(q.List.Tail.Next.Value, model.Object{Value: tt.expected[len(tt.expected)-1]}) {
+					t.Error("Add() did not add values as expected")
 				}
 			}
 		})
