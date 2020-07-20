59e32d07d83f50b1a088f29fbfdf2aae3b84241b
mrkish <ahoythar@gmail.com>
2020-07-19 22:22:48 -0500

    starting to flesh out queue and deque implementations

---
+++ deque/deque.go
@@ -0,0 +1,50 @@
+package deque
+
+import (
+	"errors"
+	"go-datastructures/linkedlist"
+	"go-datastructures/model"
+)
+
+// Deque :: struct :: FILO collection
+type Deque struct {
+	List *linkedlist.DoublyLinkedList
+}
+
+// New :: func :: Returns pointer to a new Deque
+func New(values ...string) *Deque {
+	l := linkedlist.NewDoublyLinked(values...)
+	return &Deque{
+		List: l,
+	}
+}
+
+// Dequeue :: func :: returns the first value in the Queue,
+// and removes that value from the embedded LinkedList
+func (d *Deque) Dequeue() (model.Object, error) {
+	if d.List.Head != nil {
+		val := d.List.Head.Value
+		return val, d.List.Remove(val)
+	}
+	return model.Object{}, errors.New("deque is empty")
+}
+
+// AddFirst :: func :: Adds a value to the Deque in first position
+func (d *Deque) AddFirst(obj model.Object) {
+	d.List.AddHead(obj)
+}
+
+// AddLast :: func :: Adds a value to the Deque in last position
+func (d *Deque) AddLast(obj model.Object) {
+	d.List.AddTail(obj)
+}
+
+// Remove :: func :: Removes a value from the Queue
+func (d *Deque) Remove(obj model.Object) {
+	d.List.Remove(obj)
+}
+
+// PeekFirst :: func :: Returns the Deque's current value
+func (d *Deque) PeekFirst() model.Object {
+	return d.List.Current.Value
+}
--- queue/queue.go
+++ queue/queue.go
@@ -21,15 +21,25 @@ func New(values ...string) *Queue {
 
 // Dequeue :: func :: returns the first value in the Queue,
 // and removes that value from the embedded LinkedList
-func (s *Queue) Dequeue() (model.Object, error) {
-	if s.List.Head != nil {
-		val := s.List.Head.Value
-		return val, s.List.Remove(val)
+func (q *Queue) Dequeue() (model.Object, error) {
+	if q.List.Head != nil {
+		val := q.List.Head.Value
+		return val, q.List.Remove(val)
 	}
 	return model.Object{}, errors.New("queue is empty")
 }
 
 // Add :: func :: Adds a value to the Queue in last position
-func (s *Queue) Add(obj model.Object) {
-	s.List.AddTail(obj)
+func (q *Queue) Add(obj model.Object) {
+	q.List.AddTail(obj)
+}
+
+// Remove :: func :: Removes a value from the Queue
+func (q *Queue) Remove(obj model.Object) {
+	q.List.Remove(obj)
+}
+
+// Peek :: func :: Returns the Queue's current value
+func (q *Queue) Peek() model.Object {
+	return q.List.Current.Value
 }
