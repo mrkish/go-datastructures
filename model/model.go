e73c5a39912080c932329b5b4ab043ab9f390e33
Mark Johnson <mark.johnson3@centene.com>
2020-08-17 12:13:11 -0500

    added comment about why the model.Object exists

--- model/model.go
+++ model/model.go
@@ -1,5 +1,8 @@
 package model
 
+// Just a dummy object that'd ideally be a generic type, whenever Go gets those implemented.
+// Using a string as the value is a simple way to do comparisons rather than having interface{}.
+// This may get refactored since it's kinda cumbersome, even if it's "more representative" of use cases.
 type Object struct {
 	Value string
 }
